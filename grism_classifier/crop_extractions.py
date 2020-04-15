3cdb16a0a01b00653dbe8d78f3796e7249c31f23
Jennifer Cooper <31331976+jrcooper91@users.noreply.github.com>
2020-04-14 21:54:11 -0500

    Add files via upload

--- grism_classifier/crop_extractions.py
+++ grism_classifier/crop_extractions.py
@@ -13,12 +13,29 @@ directory = "/Users/jennifercooper/Projects/thesis/23.3_mag/all/Extractions/imag
 #print(im.size)
 count = 0
 # Setting the points for cropped image
+#stellar map lin box paramters
+left0 = 1
+top0 = 1
+right0 = 301
+bottom0 = 310
 # First emission line box parameters
-left = 325
-top = 1
-right = 607
-bottom = 310
-output_dir = "/Users/jennifercooper/Projects/thesis/23.3_mag/all/Extractions/images/single/"
+left1 = 325
+top1 = 1
+right1 = 607
+bottom1 = 310
+# Second emission line
+left2 = 607
+top2 = 1
+right2 = 907
+bottom2 = 310
+# Third emission line
+left3 = 907
+top3 = 1
+right3 = 1207
+bottom3 = 310
+#where to save the files
+output_dir_em = "/Users/jennifercooper/Projects/thesis/23.3_mag/all/Extractions/images/single/" #emission line map extractions
+output_dir_st = "/Users/jennifercooper/Projects/thesis/23.3_mag/all/Extractions/images/stellar/" #emission line map stellar
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
@@ -27,6 +44,12 @@ for file in os.listdir(directory):
         # (It will not change orginal image)
         filepath = os.path.join(directory,filename)
         im = Image.open(filepath)
-        im1 = im.crop((left, top, right, bottom)).save(output_dir+"crop1_"+file)
+        width, height = im.size
+        if width in range(301,1600):
+            im1 = im.crop((left1, top1, right1, bottom1)).save(output_dir_em+"crop1em_"+file)
+        if width in range(601,1600):
+            im2 = im.crop((left2, top2, right2, bottom2)).save(output_dir_em+"crop2em_"+file)
+        if width in range(901,1600):
+            im3 = im.crop((left3, top3, right3, bottom3)).save(output_dir_em+"crop3em_"+file)
 # Shows the image in image viewer 
 #im1.show() 
